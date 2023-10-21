3959aec31869d17f9d003262ffc816ea931dd801
Ahmed Osama <ahmed.osama8282@gmail.com>
2023-10-21 17:45:35 +0300

    Update linear convolution code

--- linearConvolution.m
+++ linearConvolution.m
@@ -1,21 +1,16 @@
 %Linear Convolution
-%% Overlap Save 
 Fs = 8000;
 Ts = 1/Fs;
 t = 0:Ts:1;
 f = 100;
 sinwave = sin(2*pi*f*t);
+%% Overlap Save 
 load("Overlap Save\h.mat");
 output = conv(sinwave, h);
 subplot(2,1,1);
 plot(t, output(1:length(t)));
 title("overlap save");
 %% Overlap Add
-Fs = 8000;
-Ts = 1/Fs;
-t = 0:Ts:1;
-f = 100;
-sinwave = sin(2*pi*f*t);
 load("Overlap Add\h.mat");
 output = conv(sinwave, h);
 subplot(2,1,2);
